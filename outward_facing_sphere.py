719061ed92a36a743374054a87f14bcf1e41f476
Matthew Turk <matthewturk@gmail.com>
2024-10-07 21:16:54 -0500

    Slowly adding some other ideas

--- outward_facing_sphere.py
+++ outward_facing_sphere.py
@@ -1,6 +1,9 @@
 import nodetree_script as ns
 import nodetree_script.api.dynamic.geometry as gs
 
+# https://i.sstatic.net/8tkSm.png
+# https://blender.stackexchange.com/questions/271014/how-do-i-set-a-very-specific-number-of-instances-to-distribute-within-a-randomiz
+
 
 @ns.tree("Outward Sphere")
 def outward_facing_sphere():
@@ -14,3 +17,20 @@ def outward_facing_sphere():
         ),
         "Points": points.points,
     }
+
+
+@ns.tree("Sphere To Curves")
+def sphere_to_curves():
+    sphere1 = gs.uv_sphere(radius=100.0, segments=1024, rings=1024)
+    sphere2 = gs.uv_sphere(radius=1.0, segments=32, rings=32)
+    target_volume = gs.volume_cube(min=(-210, -210, -10), max=(-190, -190, 10))
+    points1 = gs.distribute_points_on_faces(mesh=sphere1.mesh, density=0.01)
+    points2 = gs.distribute_points_in_volume(volume=target_volume, density=0.01)
+    g1 = gs.instance_on_points(points=points1.points, instance=sphere2.mesh)
+    g2 = gs.instance_on_points(points=points2, instance=sphere2.mesh)
+    return gs.join_geometry(geometry=[g1, g2])
+
+
+@ns.tree("Max Points")
+def max_points(geometry: ns.Geometry):
+    return
