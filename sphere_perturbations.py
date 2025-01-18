211e7bba6a6d141b5b336d514178df107dbfd2cb
Matthew Turk <matthewturk@gmail.com>
2025-01-18 13:04:09 -0600

    Sphere perturbations working better

--- sphere_perturbations.py
+++ sphere_perturbations.py
@@ -4,23 +4,28 @@ import nodetree_script.api.dynamic.shader as ss
 
 
 @ns.tree("Time Varying Perturbed Sphere")
-def time_varying_perturbed_sphere(geometry: ns.Geometry, time: ns.Float):
+def time_varying_perturbed_sphere(geometry: ns.Geometry, magnitude: ns.Float = 0.1):
     ts = gs.scene_time().seconds
-    pos = gs.position()
+    factor = (ts % 2) / 2.0
+    index = gs.math(operation=gs.Math.Operation.FLOOR, value=(ts / 2))
     r1 = gs.random_value(
-        data_type=gs.RandomValue.DataType.FLOAT_VECTOR, min=-0.1, max=0.1, seed=0
+        data_type=gs.RandomValue.DataType.FLOAT_VECTOR,
+        max=magnitude,
+        seed=index,
     )
     r2 = gs.random_value(
-        data_type=gs.RandomValue.DataType.FLOAT_VECTOR, min=-0.1, max=0.1, seed=1
+        data_type=gs.RandomValue.DataType.FLOAT_VECTOR,
+        max=magnitude,
+        seed=index + 1,
     )
-    posi = r1 + pos
-    posf = r2 + pos
-    factor = (ts % 2) / 5
+    sphere = gs.subdivide_mesh(mesh=geometry, level=4).set_position(
+        position=gs.vector_math(
+            operation=gs.VectorMath.Operation.NORMALIZE, vector=gs.position()
+        )
+        + gs.mix(data_type=gs.Mix.DataType.VECTOR, factor=factor, a=r1, b=r2),
+    )
     return gs.set_position(
-        geometry=geometry,
-        position=gs.mix(
-            data_type=gs.Mix.DataType.VECTOR, factor=factor, a=posi, b=posf
-        ),
+        geometry=sphere,
     )
 
 
